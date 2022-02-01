dad0573a95e52a3ac8fcc53453ee9228398c4b24
Carter Biliato <cbiliato@uoguelph.ca>
2022-02-01 09:09:16 -0500

    Added new code for branch

---
+++ src/javalabs/branchPractice.java
@@ -0,0 +1,17 @@
+package javalabs;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carts
+ */
+public class branchPractice {
+    public static void main(String[] args) {
+        System.out.println("Branch Practice!");
+    }
+    
+}
